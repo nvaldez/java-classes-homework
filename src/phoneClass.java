9d70f6df43c689bf2c1da4cb57d2303034f845a8
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2020-05-04 10:41:33 -0500

    created setters and getters

--- src/phoneClass.java
+++ src/phoneClass.java
@@ -23,4 +23,34 @@ public class phoneClass {
 		this.model = model;
 	}
 
+	public String getName(){
+		return this.name;
+	}
+
+	public String getBrand(){
+		return this.brand;
+	}
+
+	public String getModel(){
+		return this.model;
+	}
+	 public boolean getHasWarranty(){
+		return this.hasWarranty;
+	 }
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public void setHasWarranty(boolean hasWarranty) {
+		this.hasWarranty = hasWarranty;
+	}
 }
