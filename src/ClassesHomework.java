4d9d1b537e910b47f34a2093d95b14b5e16440f0
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2020-05-04 10:20:11 -0500

    changed pet type

--- src/ClassesHomework.java
+++ src/ClassesHomework.java
@@ -5,7 +5,7 @@ public class ClassesHomework {
 		// TODO Auto-generated method stub
 		
 		// Testing the pet class
-		Pet pet = new Pet("Redondo", 1, "Gretna", "Rottweiler");
+		Pet pet = new Pet("Redondo", 1, "Gretna", "Dog");
 		
 		System.out.println("Pet name is: " + pet.getName());
 		System.out.println("Pet age is: " + pet.getAge());
@@ -19,7 +19,7 @@ public class ClassesHomework {
 		pet.setName("Jadiel");
 		pet.setAge(8);
 		pet.setLocation("NOLA");
-		pet.setType("Pitbull");
+		pet.setType("Cat");
 		
 		System.out.println("Pet name is: " + pet.getName());
 		System.out.println("Pet age is: " + pet.getAge());
